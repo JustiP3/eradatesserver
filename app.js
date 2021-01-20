840136d9ac74fdc3578d866a1c31f464a876a835
justin <justinphil02@yahoo.com>
2021-01-20 11:19:12 -0800

    add era and typeeras table init and test data. add route for get all eras

--- app.js
+++ app.js
@@ -24,20 +24,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+//DB Request API Routes
+
 app.get("/api/datetypes", (req, res, next) => {
   var sql = "select * from datetype"
   var params = []
-  db.all(sql, params, (err, rows) => {
-      if (err) {
-        res.status(400).json({"error":err.message});
-        return;
-      }
-      res.json({
-          "message":"success",
-          "data":rows
-      })
-    });
+  selectAll(sql, params, err, rows)
 });
+app.get("/api/eras", (req, res, next) => {
+  var sql = "select * from era"
+  var params = []
+  selectAll(sql, params, err, rows)
+});
+
+// SQL Helper functions
+
+const selectAll = (sql, params, err, rows) => {
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      res.status(400).json({"error":err.message});
+      return;
+    }
+    res.json({
+        "message":"success",
+        "data":rows
+    })
+  });
+}
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- database.js
+++ database.js
@@ -8,7 +8,11 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
       console.error(err.message)
       throw err
     }else{
+        // database is open 
+
         console.log('Connected to the SQLite database.')
+
+        //create datetype table
         db.run(`CREATE TABLE datetype (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name text UNIQUE, 
@@ -17,14 +21,59 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
         (err) => {
             if (err) {
                 // Table already created
-            }else{
+            } else {
                 // Table just created, creating some rows
-                var insert = 'INSERT INTO datetype (name) VALUES (?)'
+                const insert = 'INSERT INTO datetype (name) VALUES (?)'
                 db.run(insert, ["picnic"])
                 db.run(insert, ["disco"])
+                
+            }
+        });
+
+       
+        //create era table
+        db.run(`CREATE TABLE era (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            era text UNIQUE,
+            CONSTRAINT era_unique UNIQUE (era)
+        )`,
+        (err) => {
+            if (err) {
+                //Table already created
+            } else {
+                //Table just created, adding some rows
+                const insert = 'INSERT INTO era (era) VALUES (?)'
+                db.run(insert, ["1920s"])
+                db.run(insert, ["1930s"])
+                db.run(insert, ["1940s"])
+                db.run(insert, ["1950s"])
+                db.run(insert, ["1960s"])
+                db.run(insert, ["1970s"])
+                db.run(insert, ["1980s"])
+                db.run(insert, ["1990s"])
+                db.run(insert, ["2000s"])
                 console.log("added some rows into database")
             }
-        });  
+        });
+
+        //create join table 
+        db.run(`CREATE TABLE typeeras (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            era_id INTEGER,
+            type_id INTEGER
+        )`,
+        (err) => {
+            if (err) {
+                //Table already created
+            } else {
+                //Table just created, adding some rows
+                const insert = 'INSERT INTO typeeras (era_id, type_id) VALUES (?, ?)'
+                db.run(insert, ["1", "1"])
+                db.run(insert, ["2", "7"])               
+            }
+        });
+
+        // database is open
     }
   
 });
